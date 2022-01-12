2a486dd4e8dd5cdb3461d3fcabb27513e75caa13
steverendell <steve.rendell@syscon.net>
2022-01-12 13:04:39 +0000

    Initial version of JPA mappings for visits

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderBookingRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderBookingRepository.kt
@@ -7,8 +7,9 @@ import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderBooking
 import java.util.Optional
 
 @Repository
-interface OffenderBookingRepository : PagingAndSortingRepository<OffenderBooking, Long>,
-  JpaSpecificationExecutor<OffenderBooking> {
+interface OffenderBookingRepository :
+  PagingAndSortingRepository<OffenderBooking, Long>, JpaSpecificationExecutor<OffenderBooking> {
+
   fun findByOffenderNomsIdAndActive(nomsId: String, active: Boolean): Optional<OffenderBooking>
   fun findByOffenderNomsIdAndBookingSequence(nomsId: String, bookingSequence: Int): Optional<OffenderBooking>
 }
