78261b99fbde9e9e15df7439ce9af3207b1ea617
Peter Phillips <peter@greenthistle.co.uk>
2022-01-12 07:57:27 +0000

    NN-3552: ⬆️  Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
   kotlin("plugin.spring") version "1.6.10"
 }
 
@@ -14,7 +14,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.flywaydb:flyway-core:8.4.0")
+  implementation("org.flywaydb:flyway-core:8.4.1")
   implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
@@ -23,7 +23,7 @@ dependencies {
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 
-  implementation("com.zaxxer:HikariCP:5.0.0")
+  implementation("com.zaxxer:HikariCP:5.0.1")
   runtimeOnly("com.h2database:h2:2.0.206")
   runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.13.0.0.1")
 
@@ -32,7 +32,6 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.mockito:mockito-inline:4.2.0")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.29")
 }
 
