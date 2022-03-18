4dc41b1422adc60bb4a10c18bb4a50b10b9fe907
Peter Phillips <peter@greenthistle.co.uk>
2022-03-18 10:07:33 +0000

    SDI-123: ⬆️  Fix owasp builds by bumping versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.5-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.0"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
   idea
@@ -16,25 +16,25 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.flywaydb:flyway-core:8.5.2")
+  implementation("org.flywaydb:flyway-core:8.5.4")
   implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
 
   implementation("com.zaxxer:HikariCP:5.0.1")
   runtimeOnly("com.h2database:h2:2.1.210")
-  runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.13.0.0.1")
+  runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.14.0.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.mockito:mockito-inline:4.4.0")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.30")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.31")
   testImplementation("org.springframework.security:spring-security-test")
 }
 
