cef6f08b682e968192b044ce50363fea0503840a
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2022-07-04 15:49:22 +0100

    SDI-253 add missing sequences

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/AgencyVisitSlot.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/AgencyVisitSlot.kt
@@ -6,9 +6,11 @@ import java.util.Objects
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
+import javax.persistence.SequenceGenerator
 import javax.persistence.Table
 
 @Entity
@@ -16,6 +18,8 @@ import javax.persistence.Table
 @BatchSize(size = 25)
 data class AgencyVisitSlot(
   @Id
+  @SequenceGenerator(name = "AGENCY_VISIT_SLOT_ID", sequenceName = "AGENCY_VISIT_SLOT_ID", allocationSize = 1)
+  @GeneratedValue(generator = "AGENCY_VISIT_SLOT_ID")
   @Column(name = "AGENCY_VISIT_SLOT_ID")
   var id: Long = 0,
 
