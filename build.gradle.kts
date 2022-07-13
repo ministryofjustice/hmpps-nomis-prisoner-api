fa222442cc67dcfa7d727fdd98baa75172c978dc
petergphillips <peter@greenthistle.co.uk>
2022-07-13 10:12:37 +0100

    Revert "SDI-268: ⬆️ Kotlin upgrade (#93)"
    This reverts commit b68a76fef34ebe59b86886f5f613352047fffe4e.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.0-beta"
-  kotlin("plugin.spring") version "1.7.10"
-  kotlin("plugin.jpa") version "1.7.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.2"
+  kotlin("plugin.spring") version "1.7.0"
+  kotlin("plugin.jpa") version "1.7.0"
   idea
 }
 
