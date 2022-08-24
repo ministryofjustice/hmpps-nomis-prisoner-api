a531d65b95164728d3e91b3c04ec121bfe03b88f
Peter Phillips <peter@greenthistle.co.uk>
2022-08-24 09:06:02 +0100

    SDI-348: ⬆️  Upgrade plugin to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.1-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.2"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
   idea
@@ -17,11 +17,11 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
   implementation("org.flywaydb:flyway-core")
-  implementation("com.vladmihalcea:hibernate-types-52:2.17.0")
+  implementation("com.vladmihalcea:hibernate-types-52:2.18.0")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.11")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
 
@@ -33,8 +33,8 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.6.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.1")
+  testImplementation("org.mockito:mockito-inline:4.7.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.2")
   testImplementation("org.springframework.security:spring-security-test")
 }
 
