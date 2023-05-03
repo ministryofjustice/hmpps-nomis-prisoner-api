53820024c0812d36831249de16b63d0ae4e21153
Mike Halma <mike.halma@digital.justice.gov.uk>
2023-05-03 16:45:03 +0100

    SDIT-412 Replace empty @Bean

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/config/OpenApiConfiguration.kt
@@ -71,4 +71,9 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
       }
     }
   }
+
+  @Bean
+  fun enableLocalTimePrimitiveType() {
+    PrimitiveType.enablePartialTime()
+  }
 }
