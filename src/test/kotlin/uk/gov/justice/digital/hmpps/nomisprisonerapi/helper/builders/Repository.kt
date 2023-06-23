657dd09e03b5e974d2b6217d7121e0b643d5afe5
Andy Marke <andy.marke@digital.justice.gov.uk>
2023-06-23 11:31:40 +0100

    SDIT-856 Parties test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helper/builders/Repository.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helper/builders/Repository.kt
@@ -280,6 +280,7 @@ class Repository(
     visit.visitStatus = visitStatusRepository.findById(VisitStatus.NORM).orElseThrow()
   }
 
+  @Suppress("SqlWithoutWhere")
   fun updateCreatedToMatchVisitStart() {
     val sql = "UPDATE offender_visits SET CREATE_DATETIME = START_TIME"
     jdbcTemplate.execute(sql)
@@ -358,8 +359,8 @@ class Repository(
   fun lookupIncidentType(): AdjudicationIncidentType =
     adjudicationIncidentTypeRepository.findByIdOrNull(AdjudicationIncidentType.pk(AdjudicationIncidentType.GOVERNORS_REPORT))!!
 
-  fun lookupActionDecision(): IncidentDecisionAction =
-    incidentDecisionActionRepository.findByIdOrNull(IncidentDecisionAction.pk(IncidentDecisionAction.PLACED_ON_REPORT_ACTION_CODE))!!
+  fun lookupActionDecision(code: String = IncidentDecisionAction.PLACED_ON_REPORT_ACTION_CODE): IncidentDecisionAction =
+    incidentDecisionActionRepository.findByIdOrNull(IncidentDecisionAction.pk(code))!!
 
   fun lookupAdjudicationOffence(code: String): AdjudicationIncidentOffence =
     adjudicationIncidentOffenceRepository.findByCode(code)!!
@@ -398,6 +399,11 @@ class Repository(
       incident.repairs.addAll(
         adjudicationIncidentBuilder.repairs.mapIndexed { index, repair ->
           repair.build(incident, index + 1, lookupRepairType(repair.repairType))
+        },
+      )
+      incident.parties.addAll(
+        adjudicationIncidentBuilder.parties.mapIndexed { index, party ->
+          party.build(incident, lookupActionDecision(party.actionDecision), index + 1)
         },
       )
     }
