e57c1bfc372bc3819068018b2dda38882f44bd84
Andy Marke <andy.marke@digital.justice.gov.uk>
2023-06-27 12:41:02 +0100

    SDIT-905 VSIP created rooms have parent and not based on name

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/AgencyInternalLocationRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/AgencyInternalLocationRepository.kt
@@ -7,25 +7,8 @@ import java.util.Optional
 
 @Repository
 interface AgencyInternalLocationRepository : JpaRepository<AgencyInternalLocation, Long> {
-  fun findAgencyInternalLocationsByAgencyIdAndLocationTypeAndActive(
-    agencyId: String,
-    locationType: String,
-    active: Boolean,
-  ): List<AgencyInternalLocation>
-
-  fun findAgencyInternalLocationsByAgencyIdAndLocationType(
-    agencyId: String,
-    locationType: String,
-  ): List<AgencyInternalLocation>
 
   fun findOneByDescription(description: String): Optional<AgencyInternalLocation>
-  fun findOneByDescriptionAndAgencyId(description: String, agencyId: String): Optional<AgencyInternalLocation>
   fun findByDescriptionAndAgencyId(description: String, agencyId: String): AgencyInternalLocation?
-  fun findOneByLocationId(locationId: Long): Optional<AgencyInternalLocation>
-  fun findByLocationCodeAndAgencyId(locationCode: String, agencyId: String): List<AgencyInternalLocation>
-  fun findByAgencyIdAndLocationTypeAndActiveAndParentLocationIsNull(
-    agencyId: String,
-    locationType: String,
-    active: Boolean,
-  ): List<AgencyInternalLocation>
+  fun findByAgencyIdAndActiveAndLocationCodeInAndParentLocationIsNull(agencyId: String, active: Boolean = true, locationCodes: List<String> = listOf("VISIT", "VISITS")): AgencyInternalLocation?
 }
