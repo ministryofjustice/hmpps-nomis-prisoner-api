ed3b44fef3b74e4a63fde2837b52e9f04f240e68
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2023-09-19 11:53:55 +0100

    SDIT-997 Add migration endpoints (#461)
    Change ordering to use nomsId, not numerical id
    
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/specification/NonAssociationSpecification.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/specification/NonAssociationSpecification.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.criteria.CriteriaQuery
 import jakarta.persistence.criteria.Predicate
 import jakarta.persistence.criteria.Root
 import org.springframework.data.jpa.domain.Specification
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.Offender
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderNonAssociation
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderNonAssociationDetail
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderNonAssociationId
@@ -30,12 +31,17 @@ class NonAssociationSpecification(private val filter: NonAssociationFilter) : Sp
       predicates.add(criteriaBuilder.lessThanOrEqualTo(getEffectiveDate, this))
     }
 
-    // Filter out duplicates by only returning the id - ordered record for each pair of offenders
+    // Filter out duplicates by only returning the "nomsId1 < nomsId2" ordered record for each pair of offenders
     predicates.add(
       criteriaBuilder.lessThan(
-        root.get<String>(OffenderNonAssociation::id.name).get<String>(OffenderNonAssociationId::offender.name),
-        root.get<String>(OffenderNonAssociation::id.name)
-          .get<String>(OffenderNonAssociationId::nsOffender.name),
+        root
+          .get<String>(OffenderNonAssociation::id.name)
+          .get<String>(OffenderNonAssociationId::offender.name)
+          .get<String>(Offender::nomsId.name),
+        root
+          .get<String>(OffenderNonAssociation::id.name)
+          .get<String>(OffenderNonAssociationId::nsOffender.name)
+          .get<String>(Offender::nomsId.name),
       ),
     )
 
