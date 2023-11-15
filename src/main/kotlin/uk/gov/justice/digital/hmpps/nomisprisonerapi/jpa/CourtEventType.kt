503196bdb8822b3b4ea20b9a3a9d736e814d9884
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2023-11-15 16:35:14 +0000

    SDIT-1226 initial court event mappings

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/CourtEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/CourtEventType.kt
@@ -9,7 +9,6 @@ class CourtEventType(code: String, description: String) : ReferenceCode(MOVE_RSN
   companion object {
     const val MOVE_RSN = "MOVE_RSN"
 
-    // ... but entries of type APP and class INT_MOV are all from domain INT_SCH_RSN
     fun pk(code: String): Pk = Pk(MOVE_RSN, code)
   }
 }
