73efe7465099eec55b199c0b50712dc17d79e8bc
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2023-11-16 15:05:46 +0000

    SDIT-1226 reference data

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/Statute.kt
@@ -0,0 +1,32 @@
+package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.Hibernate
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.helper.EntityOpen
+import java.util.Objects
+
+@Entity
+@Table(name = "STATUTES")
+@EntityOpen
+class Statute(
+  @Id
+  @Column(name = "STATUTE_CODE")
+  val code: String,
+
+  val description: String,
+) {
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+    other as Statute
+    return code == other.code
+  }
+
+  override fun hashCode(): Int {
+    return Objects.hashCode(code)
+  }
+}
