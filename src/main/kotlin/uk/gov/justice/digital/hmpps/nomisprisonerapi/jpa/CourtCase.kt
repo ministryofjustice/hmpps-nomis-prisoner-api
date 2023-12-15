25f5d3ce93cec94381443315a69a0d30b838decb
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2023-12-15 14:00:33 +0000

    SDIT-1328 court case create (#554)
    * SDIT-1328 court case create
    
    * SDIT-1328 court case create
    
    ---------
    
    Co-authored-by: lbennett-moj <lucy.bennett@digital .justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/CourtCase.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/CourtCase.kt
@@ -47,7 +47,7 @@ class CourtCase(
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "AGY_LOC_ID", nullable = false)
-  val prison: AgencyLocation,
+  val court: AgencyLocation,
 
   // optional and without referential integrity on DB but no nulls and all reference codes 100% match
   @ManyToOne
@@ -83,7 +83,7 @@ class CourtCase(
 
   @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "COMBINED_CASE_ID")
-  val combinedCase: CourtCase?,
+  val combinedCase: CourtCase? = null,
 
   val statusUpdateComment: String? = null,
 
