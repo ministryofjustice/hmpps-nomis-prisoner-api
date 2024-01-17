72e6dbfeb97f903909a88ccf0486b53def6c2ab0
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2024-01-17 13:48:14 +0000

    SDIT-1422 offence result and charge status logic

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/sentencing/SentencingResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/sentencing/SentencingResourceIntTest.kt
@@ -1432,7 +1432,7 @@ class SentencingResourceIntTest : IntegrationTestBase() {
           .jsonPath("courtEvents[1].directionCode.code").isEqualTo("OUT")
           .jsonPath("courtEvents[1].judgeName").doesNotExist()
           .jsonPath("courtEvents[1].courtId").isEqualTo("ABDRCT")
-          .jsonPath("courtEvents[1].outcomeReasonCode.code").isEqualTo("1002")
+          .jsonPath("courtEvents[1].outcomeReasonCode.code").isEqualTo("1004")
           .jsonPath("courtEvents[1].commentText").doesNotExist()
           .jsonPath("courtEvents[1].orderRequestedFlag").isEqualTo(false)
           .jsonPath("courtEvents[1].holdFlag").doesNotExist()
