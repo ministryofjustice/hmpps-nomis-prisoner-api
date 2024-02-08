04e5a1022c0ea7e338011003c495d877e72b489b
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-02-08 12:22:20 +0000

    SDIT-1335 Implement hmpps-kotlin-spring-boot-starter lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,9 @@ configurations {
 }
 
 dependencies {
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.0.5")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("org.springframework.boot:spring-boot-starter-cache")
 
   implementation("org.flywaydb:flyway-core")
   implementation("com.vladmihalcea:hibernate-types-60:2.21.1")
