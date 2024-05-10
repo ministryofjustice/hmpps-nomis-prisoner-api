cc19a7aa7d9b690a5a1860dbf44db3ef1a4c7bd1
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2024-05-10 16:03:25 +0100

    SDIT-1734 create sentence terms (#712)
    Co-authored-by: lbennett-moj <lucy.bennett@digital .justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderSentenceTermRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderSentenceTermRepository.kt
@@ -9,5 +9,5 @@ import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderSentenceTermId
 @Repository
 interface OffenderSentenceTermRepository : JpaRepository<OffenderSentenceTerm, OffenderSentenceTermId> {
   @Query(value = "SELECT NVL(MAX(TERM_SEQ)+1, 1) FROM OFFENDER_SENTENCE_TERMS oos WHERE OFFENDER_BOOK_ID = :offenderBookId and SENTENCE_SEQ = :sentenceSeq", nativeQuery = true)
-  fun getNextTermSequence(offenderBookId: Long): Int
+  fun getNextTermSequence(offenderBookId: Long, sentenceSeq: Long): Long
 }
