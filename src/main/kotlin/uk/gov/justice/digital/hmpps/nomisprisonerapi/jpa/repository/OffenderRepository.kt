c8f813a6c9748c15b6a63749b86351f5d778f20d
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-05-17 17:08:16 +0100

    SDIT-1753 Improve performance of get prisoner ids
    Previous version did an n+1 to load the booking

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderRepository.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Query
@@ -18,6 +20,23 @@ interface OffenderRepository : JpaRepository<Offender, Long>, JpaSpecificationEx
 
   @Query("select o from Offender o WHERE o.nomsId = :nomsId and o.rootOffenderId = o.id")
   fun findRootByNomsId(nomsId: String): Offender?
+
+  @Query(
+    value = """
+        select o.nomsId as nomsId, ob.bookingId as bookingId, ob.active as active, ob.inOutStatus as inOutStatus from OffenderBooking ob join ob.offender.rootOffender o where ob.rootOffender = o.rootOffender and ob.bookingSequence = 1
+    """,
+    countQuery = """
+      select count(ob) from OffenderBooking ob where ob.bookingSequence = 1
+    """,
+  )
+  fun findAllWithBookings(pageable: Pageable): Page<PrisonerId>
 }
 
 fun OffenderRepository.findLatestAliasByNomisId(nomsId: String): Offender? = findByNomsIdOrderedWithBookings(nomsId).firstOrNull()
+
+interface PrisonerId {
+  fun getNomsId(): String
+  fun getBookingId(): Long
+  fun isActive(): Boolean
+  fun getInOutStatus(): String
+}
