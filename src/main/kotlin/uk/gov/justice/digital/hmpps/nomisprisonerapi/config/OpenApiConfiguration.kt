15427daa95c982cbbc71ec0f6901d7fcddb930fc
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-05-21 10:09:06 +0100

    SDIT-1766: 🔧 Update urls to point to cloud platform (#726)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/config/OpenApiConfiguration.kt
@@ -25,8 +25,8 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
     .servers(
       listOf(
         Server().url("https://nomis-prisoner-api-dev.prison.service.justice.gov.uk").description("Development"),
-        Server().url("https://nomis-prsner-pp.aks-live-1.studio-hosting.service.justice.gov.uk").description("PreProd"),
-        Server().url("https://nomis-prisoner.aks-live-1.studio-hosting.service.justice.gov.uk").description("Prod"),
+        Server().url("https://nomis-prisoner-api-preprod.prison.service.justice.gov.uk").description("PreProd"),
+        Server().url("https://nomis-prisoner-api.prison.service.justice.gov.uk").description("Prod"),
         Server().url("http://localhost:8080").description("Local"),
       ),
     )
