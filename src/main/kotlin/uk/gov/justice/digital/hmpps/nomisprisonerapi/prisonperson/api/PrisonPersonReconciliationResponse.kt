fc424dc7ce1452c5e13c078640985559a3a01532
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-08-21 11:56:40 +0100

    SDIT-1829 Add get physical attributes for reconciliation endpoint (#808)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/prisonperson/api/PrisonPersonReconciliationResponse.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.nomisprisonerapi.prisonperson.api
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Reconciliation details held against a prisoner")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class PrisonPersonReconciliationResponse(
+  @Schema(description = "The prisoner's unique identifier", example = "A1234AA")
+  val offenderNo: String,
+  @Schema(description = "The height of the prisoner in centimetres", example = "180")
+  val height: Int? = null,
+  @Schema(description = "The weight of the prisoner in kilograms", example = "80")
+  val weight: Int? = null,
+)
