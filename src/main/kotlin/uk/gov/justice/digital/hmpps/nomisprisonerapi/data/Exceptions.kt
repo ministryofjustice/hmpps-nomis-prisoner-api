cf08c0282a64da9d5a8faeb620afe3c00a5636fa
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-09-04 10:30:29 +0100

    SDIT-2026 Return existing visitId for 409 duplicate (#817)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/data/Exceptions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/data/Exceptions.kt
@@ -2,10 +2,10 @@ package uk.gov.justice.digital.hmpps.nomisprisonerapi.data
 
 import java.util.function.Supplier
 
-class BadDataException(message: String?, val error: BadRequestError? = null) : RuntimeException(message), Supplier<BadDataException> {
-  override fun get(): BadDataException {
-    return BadDataException(message, error)
-  }
+class BadDataException(message: String?, val error: BadRequestError? = null) :
+  RuntimeException(message),
+  Supplier<BadDataException> {
+  override fun get(): BadDataException = BadDataException(message, error)
 }
 
 enum class BadRequestError(val errorCode: Int) {
@@ -13,14 +13,14 @@ enum class BadRequestError(val errorCode: Int) {
   PRISONER_MOVED_ALLOCATION_ENDED(1002),
 }
 
-class ConflictException(message: String?) : RuntimeException(message), Supplier<ConflictException> {
-  override fun get(): ConflictException {
-    return ConflictException(message)
-  }
+class ConflictException(message: String?, val entityId: String? = null) :
+  RuntimeException(message),
+  Supplier<ConflictException> {
+  override fun get(): ConflictException = ConflictException(message, entityId)
 }
 
-class NotFoundException(message: String?) : RuntimeException(message), Supplier<NotFoundException> {
-  override fun get(): NotFoundException {
-    return NotFoundException(message)
-  }
+class NotFoundException(message: String?) :
+  RuntimeException(message),
+  Supplier<NotFoundException> {
+  override fun get(): NotFoundException = NotFoundException(message)
 }
