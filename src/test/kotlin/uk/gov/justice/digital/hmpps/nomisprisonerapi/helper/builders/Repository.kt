131bbd0b747793766e7a4286644c3ee2cce0a1b6
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-09-17 10:26:44 +0100

    SDIT-2044 Move visit tests to use DSL (#835)
    Since visits creates person and contact records moving exiting tests to use DSL makes it easier for teh contact work

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helper/builders/Repository.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helper/builders/Repository.kt
@@ -12,7 +12,6 @@ import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.AgencyVisitDay
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.AgencyVisitSlot
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.AgencyVisitTime
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.CSIPReport
-import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.ContactType
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.CourseActivity
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.CourseSchedule
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.CourtCase
@@ -36,7 +35,6 @@ import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.PayBand
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.Person
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.Questionnaire
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.ReferenceCode.Pk
-import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.RelationshipType
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.SentenceAdjustment
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.SentenceCalculationType
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.SentenceCalculationTypeId
@@ -45,7 +43,6 @@ import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.Staff
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.StaffUserAccount
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.Visit
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.VisitStatus
-import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.VisitType
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.ActivityRepository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.AdjudicationHearingRepository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.AdjudicationIncidentPartyRepository
@@ -85,15 +82,12 @@ import kotlin.jvm.optionals.getOrNull
 @Transactional
 class Repository(
   val genderRepository: ReferenceCodeRepository<Gender>,
-  val contactTypeRepository: ReferenceCodeRepository<ContactType>,
-  val relationshipTypeRepository: ReferenceCodeRepository<RelationshipType>,
   val offenderRepository: OffenderRepository,
   val agencyLocationRepository: AgencyLocationRepository,
   val agencyInternalLocationRepository: AgencyInternalLocationRepository,
   val personRepository: PersonRepository,
   val visitRepository: VisitRepository,
   val visitStatusRepository: ReferenceCodeRepository<VisitStatus>,
-  val visitTypeRepository: ReferenceCodeRepository<VisitType>,
   val iepLevelRepository: ReferenceCodeRepository<IEPLevel>,
   val agencyVisitSlotRepository: AgencyVisitSlotRepository,
   val agencyVisitDayRepository: AgencyVisitDayRepository,
@@ -140,36 +134,7 @@ class Repository(
     }
 
     offenderBuilder.bookingBuilders.forEachIndexed { index, bookingBuilder ->
-      val booking = bookingBuilder.build(offender, index + 1, lookupAgency(bookingBuilder.agencyLocationId))
-      bookingBuilder.visitBalanceBuilder?.run {
-        booking.visitBalance = this.build(booking)
-      }
-      booking.contacts.addAll(
-        bookingBuilder.contacts.map {
-          it.build(booking, lookupContactType(it.contactTypeCode), lookupRelationshipType(it.relationshipTypeCode))
-        },
-      )
-      booking.visits.addAll(
-        bookingBuilder.visits.map { visitBuilder ->
-          val visit = visitBuilder.build(
-            offenderBooking = booking,
-            visitType = lookupVisitType(visitBuilder.visitTypeCode),
-            visitStatus = lookupVisitStatus(visitBuilder.visitStatusCode),
-            agencyLocation = lookupAgency(visitBuilder.agyLocId),
-            agencyInternalLocation = visitBuilder.agencyInternalLocationDescription?.run {
-              lookupAgencyInternalLocationByDescription(
-                this,
-              )
-            },
-          )
-          visit.visitors.addAll(
-            visitBuilder.visitors.map {
-              it.build(it.person, leadVisitor = it.leadVisitor, visit)
-            } + visitBuilder.visitOutcome.build(visit),
-          )
-          visit
-        },
-      )
+      bookingBuilder.build(offender, index + 1, lookupAgency(bookingBuilder.agencyLocationId))
     }
 
     offenderRepository.saveAndFlush(offender)
@@ -216,7 +181,6 @@ class Repository(
   fun deleteOffenders() = offenderRepository.deleteAll()
   fun deleteMergeTransactions() = mergeTransactionRepository.deleteAll()
 
-  fun save(personBuilder: LegacyPersonBuilder): Person = personRepository.save(personBuilder.build())
   fun delete(people: Collection<Person>) = personRepository.deleteAllById(people.map { it.id })
 
   fun deleteProgramServices() = programServiceRepository.deleteAll()
@@ -249,7 +213,6 @@ class Repository(
     offenderChargeRepository.deleteByOffenderBookingBookingId(bookingId = bookingId)
 
   fun delete(sentence: OffenderSentence) = offenderSentenceRepository.deleteById(sentence.id)
-  fun deleteSentenceByBookingId(bookingId: Long) = offenderSentenceRepository.deleteByIdOffenderBookingBookingId(bookingId = bookingId)
 
   fun delete(questionnaire: Questionnaire) = questionnaireRepository.deleteById(questionnaire.id)
   fun delete(incident: Incident) = incidentRepository.deleteById(incident.id)
@@ -258,10 +221,6 @@ class Repository(
 
   // Builder lookups
   fun lookupGender(code: String): Gender = genderRepository.findByIdOrNull(Pk(Gender.SEX, code))!!
-  fun lookupContactType(code: String): ContactType =
-    contactTypeRepository.findByIdOrNull(Pk(ContactType.CONTACTS, code))!!
-
-  fun lookupVisitType(code: String): VisitType = visitTypeRepository.findByIdOrNull(Pk(VisitType.VISIT_TYPE, code))!!
 
   fun lookupIepLevel(code: String): IEPLevel = iepLevelRepository.findByIdOrNull(Pk(IEPLevel.IEP_LEVEL, code))!!
 
@@ -272,12 +231,6 @@ class Repository(
     sentenceCategoryTypeRepository.findByIdOrNull(Pk(SentenceCategoryType.CATEGORY, code))!!
 
   fun lookupSentenceAdjustment(code: String): SentenceAdjustment = sentenceAdjustmentRepository.findByIdOrNull(code)!!
-
-  fun lookupVisitStatus(code: String): VisitStatus =
-    visitStatusRepository.findByIdOrNull(Pk(VisitStatus.VISIT_STATUS, code))!!
-
-  fun lookupRelationshipType(code: String): RelationshipType =
-    relationshipTypeRepository.findByIdOrNull(Pk(RelationshipType.RELATIONSHIP, code))!!
 
   fun lookupAgency(id: String): AgencyLocation = agencyLocationRepository.findByIdOrNull(id)!!
   fun lookupAgencyInternalLocationByDescription(description: String): AgencyInternalLocation? =
