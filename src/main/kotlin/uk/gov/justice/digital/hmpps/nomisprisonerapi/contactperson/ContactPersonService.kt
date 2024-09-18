96661ab8616236e3ba58a2861d10e7de3fb0fe6a
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-09-18 12:32:07 +0100

    SDIT-2044 A few more fields to PERSON (#842)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/contactperson/ContactPersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/contactperson/ContactPersonService.kt
@@ -2,8 +2,10 @@ package uk.gov.justice.digital.hmpps.nomisprisonerapi.contactperson
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.alerts.NomisAudit
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.data.NotFoundException
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.data.toCodeDescription
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.Staff
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.PersonRepository
 
 @Service
@@ -20,6 +22,27 @@ class ContactPersonService(private val personRepository: PersonRepository) {
       language = it.language?.toCodeDescription(),
       interpreterRequired = it.interpreterRequired,
       domesticStatus = it.domesticStatus?.toCodeDescription(),
+      deceasedDate = it.deceasedDate,
+      isStaff = it.isStaff,
+      isRemitter = it.isRemitter,
+      keepBiometrics = it.keepBiometrics,
+      audit = NomisAudit(
+        createDatetime = it.createDatetime,
+        createUsername = it.createUsername,
+        createDisplayName = it.createStaffUserAccount?.staff.asDisplayName(),
+        modifyDatetime = it.modifyDatetime,
+        modifyUserId = it.modifyUserId,
+        modifyDisplayName = it.modifyStaffUserAccount?.staff.asDisplayName(),
+        auditUserId = it.auditUserId,
+        auditTimestamp = it.auditTimestamp,
+        auditModuleName = it.auditModuleName,
+        auditAdditionalInfo = it.auditAdditionalInfo,
+        auditClientIpAddress = it.auditClientIpAddress,
+        auditClientUserId = it.auditClientUserId,
+        auditClientWorkstationName = it.auditClientWorkstationName,
+      ),
     )
   } ?: throw NotFoundException("Person not found $personId")
 }
+
+private fun Staff?.asDisplayName(): String? = this?.let { "${it.firstName} ${it.lastName}" }
