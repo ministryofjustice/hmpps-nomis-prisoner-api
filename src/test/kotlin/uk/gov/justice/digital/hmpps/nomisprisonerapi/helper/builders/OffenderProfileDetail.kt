37fdb707d80e9ab0c8840e9e42c7d48b76887a45
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-09-25 12:19:05 +0100

    SDIT-2023 Implement upsert profile details service (#856)
    Co-authored-by: Andy Marke <andy.marke@digital.justice.gov.uk>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helper/builders/OffenderProfileDetail.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helper/builders/OffenderProfileDetail.kt
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderProfile
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderProfileDetail
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderProfileDetailId
-import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.ProfileCodeId
-import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.ProfileCodeRepository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.ProfileTypeRepository
 
 @DslMarker
@@ -25,10 +23,8 @@ class OffenderProfileDetailBuilderFactory(
 @Component
 class OffenderProfileDetailBuilderRepository(
   val profileTypeRepository: ProfileTypeRepository,
-  val profileCodeRepository: ProfileCodeRepository,
 ) {
   fun lookupProfileType(type: String) = profileTypeRepository.findByIdOrNull(type)!!
-  fun lookupProfileCode(type: String, code: String) = profileCodeRepository.findByIdOrNull(ProfileCodeId(type, code))
 }
 
 class OffenderProfileDetailBuilder(
@@ -47,7 +43,6 @@ class OffenderProfileDetailBuilder(
       profileType = repository.lookupProfileType(profileTypeId),
     ),
     offenderProfile = profile,
-    profileCode = profileCodeId?.let { repository.lookupProfileCode(profileTypeId, profileCodeId) },
     profileCodeId = profileCodeId,
     listSequence = listSequence,
   )
