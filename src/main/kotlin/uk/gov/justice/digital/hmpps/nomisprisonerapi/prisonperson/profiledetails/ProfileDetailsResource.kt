f20e357ae5e98f6cd8fc922cd070c3378cdb115b
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-09-25 13:27:29 +0100

    SDIT-2023 Implement upsert profile details API (#859)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/prisonperson/profiledetails/ProfileDetailsResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/prisonperson/profiledetails/ProfileDetailsResource.kt
@@ -4,11 +4,14 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import jakarta.validation.Valid
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.config.ErrorResponse
@@ -69,4 +72,64 @@ class ProfileDetailsResource(private val service: ProfileDetailsService) {
   fun getProfileDetails(
     @Schema(description = "Offender number", example = "A1234AA") @PathVariable offenderNo: String,
   ): PrisonerProfileDetailsResponse = service.getProfileDetails(offenderNo)
+
+  @PutMapping("/prisoners/{offenderNo}/profile-details")
+  @Operation(
+    summary = "Upsert profile details for a prisoner",
+    description = "Upserts profile details on the latest booking for a prisoner, if it exists. Requires ROLE_NOMIS_PRISON_PERSON",
+    requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+      content = [
+        Content(
+          mediaType = "application/json",
+          schema = Schema(implementation = UpsertProfileDetailsRequest::class),
+        ),
+      ],
+    ),
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Physical Attributes Updated",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = UpsertProfileDetailsResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden to access this endpoint. Requires ROLE_NOMIS_PRISON_PERSON",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Prisoner does not exist or has no bookings",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  fun upsertPhysicalAttributes(
+    @Schema(description = "Offender number", example = "A1234AA") @PathVariable offenderNo: String,
+    @RequestBody @Valid request: UpsertProfileDetailsRequest,
+  ): UpsertProfileDetailsResponse = service.upsertProfileDetails(offenderNo, request)
 }
