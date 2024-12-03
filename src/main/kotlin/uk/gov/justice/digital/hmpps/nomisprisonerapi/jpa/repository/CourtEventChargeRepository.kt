b297b04b3aef49ad879e5f636a9875a16d3f5442
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2024-12-03 13:08:51 +0000

    SDIT-2263 update court charge  with additional CEC update

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/CourtEventChargeRepository.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.CourtEventCharge
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.CourtEventChargeId
+
+@Repository
+interface CourtEventChargeRepository : JpaRepository<CourtEventCharge, CourtEventChargeId> {
+  fun findFirstByIdOffenderChargeIdOrderByLastModifiedDateTimeDesc(offenderChargeId: Long): CourtEventCharge?
+}
