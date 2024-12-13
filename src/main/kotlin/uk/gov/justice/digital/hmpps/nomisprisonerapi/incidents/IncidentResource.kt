3fa6c32b82ecfa0252c71c53569155c59454524b
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2024-12-13 13:48:36 +0000

    SDIT-1986 First Name on offender is non-nullable (#978)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/incidents/IncidentResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/incidents/IncidentResource.kt
@@ -378,7 +378,7 @@ data class Offender(
   @Schema(description = "NOMIS id")
   val offenderNo: String,
   @Schema(description = "First name of staff member")
-  val firstName: String?,
+  val firstName: String,
   @Schema(description = "Last name of staff member")
   val lastName: String,
 )
