69abc6c454cab3e726c9ce31262aab5bee9f9783
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2025-01-08 16:25:28 +0000

    SDIT-2401 Improve Nomis get case notes performance (#1004)
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderCaseNoteRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderCaseNoteRepository.kt
@@ -1,35 +1,14 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
 
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderCaseNote
 
 @Repository
 interface OffenderCaseNoteRepository : JpaRepository<OffenderCaseNote, Long> {
-//  @Suppress("ktlint:standard:function-naming")
-//  fun findAllByOffenderBooking_BookingId(bookingId: Long): List<OffenderCaseNote>
-
   @Suppress("ktlint:standard:function-naming")
+  @EntityGraph(type = EntityGraphType.FETCH, value = "offender-case-note")
   fun findAllByOffenderBooking_Offender_NomsId(offenderNo: String): List<OffenderCaseNote>
-
-  @Query(
-    """
-      select ob.bookingId from OffenderBooking ob 
-        where 
-          (:fromId is null or ob.bookingId > :fromId) and 
-          (:toId is null or ob.bookingId < :toId) and
-          (:activeOnly = false or ob.active)
-      order by ob.bookingId asc
-    """,
-    // Should use index OFFENDER_BOOKINGS_X02 on ACTIVE_FLAG, OFFENDER_BOOK_ID and not need any table access
-  )
-  fun findAllBookingIds(
-    fromId: Long?,
-    toId: Long?,
-    activeOnly: Boolean = true,
-    pageable: Pageable,
-  ): Page<Long>
 }
