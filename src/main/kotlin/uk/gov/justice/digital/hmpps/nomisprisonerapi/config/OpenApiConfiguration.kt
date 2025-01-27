0faf05f133a5e2e15a4ebbd45ff82fff3b19d87e
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-01-27 16:53:07 +0000

    SDIT-2488 Handle OpenAPI 3.1.0

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/config/OpenApiConfiguration.kt
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.media.DateTimeSchema
 import io.swagger.v3.oas.models.media.Schema
 import io.swagger.v3.oas.models.media.StringSchema
 import io.swagger.v3.oas.models.security.SecurityRequirement
@@ -55,7 +54,7 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
       val properties = schema.properties ?: mutableMapOf()
       for (propertyName in properties.keys) {
         val propertySchema = properties[propertyName]!!
-        if (propertySchema is DateTimeSchema) {
+        if (propertySchema.format == "date-time") {
           properties.replace(
             propertyName,
             StringSchema()
