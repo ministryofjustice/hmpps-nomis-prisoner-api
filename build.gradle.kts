8b32571ffabd70b8656488c01c8cae7ccdeed799
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-02-21 11:37:59 +0000

    SDIT 2593 kotlin lib full version (#1095)
    * SDIT-2593 Try out latest kotlin lib
    
    * SDIT-2593 Upgrade to full verion of kotlin lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.0-beta-2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
