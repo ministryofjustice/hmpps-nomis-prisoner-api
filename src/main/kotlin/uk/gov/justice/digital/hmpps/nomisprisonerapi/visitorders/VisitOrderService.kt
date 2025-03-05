a2d93705008fd93041923c6fde9c6907210ff78b
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-03-05 11:28:06 +0000

    SDIT-2642: ✨ Add retrieval of balance adjustment

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/visitorders/VisitOrderService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/visitorders/VisitOrderService.kt
@@ -4,12 +4,14 @@ import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.data.NotFoundException
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.OffenderBookingRepository
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.OffenderVisitBalanceAdjustmentRepository
 import java.time.LocalDate
 
 @Service
 @Transactional
 class VisitOrderService(
   val offenderBookingRepository: OffenderBookingRepository,
+  val offenderVisitBalanceAdjustmentRepository: OffenderVisitBalanceAdjustmentRepository,
 ) {
   fun getVisitOrderBalance(offenderNo: String): PrisonerVisitOrderBalanceResponse = offenderBookingRepository.findAllByOffenderNomsId(offenderNo).takeIf { it.isNotEmpty() }
     ?.let { bookings ->
@@ -21,8 +23,12 @@ class VisitOrderService(
         remainingVisitOrders = latestBooking.visitBalance?.remainingVisitOrders ?: 0,
         remainingPrivilegedVisitOrders = latestBooking.visitBalance?.remainingPrivilegedVisitOrders ?: 0,
         visitOrderBalanceAdjustments = latestBooking.visitBalanceAdjustments.filter {
-          it.adjustDate?.let { adjDate -> !adjDate.isBefore(fromDate) && !adjDate.isAfter(toDate) } ?: false
-        }.map { it.toVisitOrderBalanceAdjResponse() },
+          it.adjustDate.let { adjDate -> !adjDate.isBefore(fromDate) && !adjDate.isAfter(toDate) }
+        }.map { it.toVisitBalanceAdjustmentResponse() },
       )
     } ?: throw NotFoundException("Prisoner with offender no $offenderNo not found with any bookings")
+
+  fun getVisitBalanceAdjustment(visitBalanceAdjustmentId: Long): VisitBalanceAdjustmentResponse = offenderVisitBalanceAdjustmentRepository.findById(visitBalanceAdjustmentId)
+    .map { it.toVisitBalanceAdjustmentResponse() }
+    .orElseThrow { NotFoundException("Visit balance adjustment with id $visitBalanceAdjustmentId not found") }
 }
