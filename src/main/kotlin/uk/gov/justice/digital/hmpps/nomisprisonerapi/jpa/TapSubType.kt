d667bccb75a808cfaea13e250998de13d1830942
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-07-02 09:40:53 +0100

    SDIT-2872 Add reference data for offender movement applications (#1251)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/TapSubType.kt
@@ -0,0 +1,14 @@
+package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa
+
+import jakarta.persistence.DiscriminatorValue
+import jakarta.persistence.Entity
+
+@Entity
+@DiscriminatorValue(TapSubType.TAP_ABS_STYP)
+class TapSubType(code: String, description: String) : ReferenceCode(TAP_ABS_STYP, code, description) {
+
+  companion object {
+    const val TAP_ABS_STYP = "TAP_ABS_STYP"
+    fun pk(code: String): Pk = Pk(TAP_ABS_STYP, code)
+  }
+}
