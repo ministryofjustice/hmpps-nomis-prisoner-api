aba5e284d953eefe1ce94f5298dfde128448a41b
karenmillermoj <karen.miller3@justice.gov.uk>
2025-07-14 11:31:10 +0100

    SDIT-2940 Truncate based on allowed length and add suffix on method return

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helpers/StringUtils.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helpers/StringUtils.kt
@@ -3,20 +3,21 @@ package uk.gov.justice.digital.hmpps.nomisprisonerapi.helpers
 import com.google.common.base.Utf8
 const val SEE_DPS = "... see DPS for full text"
 
-fun String.truncateToUtf8Length(maxLength: Int, includeSeeDpsSuffix: Boolean = false): String = // ensure doesn't exceed number of bytes Oracle can take
-  if (Utf8.encodedLength(this) <= maxLength) {
+fun String.truncateToUtf8Length(maxLength: Int, includeSeeDpsSuffix: Boolean = false): String {
+  // ensure doesn't exceed the number of bytes Oracle can take - allowing for suffix to be added
+  return if (Utf8.encodedLength(this) <= maxLength) {
     this
   } else {
-    this.truncateByOneCharacterUntilFitToUtf8Length(maxLength, if (includeSeeDpsSuffix) SEE_DPS else "")
+    val suffix = if (includeSeeDpsSuffix) SEE_DPS else ""
+    this.truncateByOneCharacterUntilFitToUtf8Length(maxLength - suffix.length) + suffix
   }
-
-private fun String.truncateByOneCharacterUntilFitToUtf8Length(maxLength: Int, suffix: String): String {
+}
+private fun String.truncateByOneCharacterUntilFitToUtf8Length(maxLength: Int): String {
   // so we don't cut into a double/triple byte character while truncating check
   // we still have a valid string before returning, even if it fits the number of bytes
-  if (this.isStillValid() && Utf8.encodedLength(this + suffix) <= maxLength) return (this + suffix)
-
+  if (this.isStillValid() && Utf8.encodedLength(this) <= maxLength) return this
   // Keep knocking of the last character until it fits the number of bytes and remains a valid string
-  return this.take(this.length - 1).truncateByOneCharacterUntilFitToUtf8Length(maxLength, suffix)
+  return this.take(this.length - 1).truncateByOneCharacterUntilFitToUtf8Length(maxLength)
 }
 
 // Utf8.encodedLength will throw if the resulting String is cut at the incorrect boundary
--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helpers/TruncateToUtf8LengthTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/helpers/TruncateToUtf8LengthTest.kt
@@ -1,7 +1,10 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.helpers
 
+import com.google.common.base.Utf8
 import io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat
 import org.junit.jupiter.api.Test
+
+private const val TWO_UNICODE_CHARS = "⌘⌥"
 
 class TruncateToUtf8LengthTest {
   @Test
@@ -57,5 +60,15 @@ class TruncateToUtf8LengthTest {
   @Test
   fun `will truncate when dps suffix and too big with huge byte characters at end`() {
     assertThat("12345678901234567890123456789😀".truncateToUtf8Length(30, true)).isEqualTo("12345$SEE_DPS")
+  }
+
+  @Test
+  fun `truncation with unicode too long unicode at end`() {
+    // ... see DPS for full text = 25 chars TWO_UNICODE_CHARS have length 6 chars
+    val textTooLong = "s".repeat(17) + TWO_UNICODE_CHARS.repeat(6)
+    val result = textTooLong.truncateToUtf8Length(50, true)
+
+    assertThat(Utf8.encodedLength(result)).isEqualTo(48) // shorter than 50 because some unicode has been truncated
+    assertThat(result).isEqualTo("${"s".repeat(17) + TWO_UNICODE_CHARS}... see DPS for full text")
   }
 }
