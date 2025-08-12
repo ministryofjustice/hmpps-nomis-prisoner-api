b78349bdf7f969450cb4e01036e0488a6523ad3a
andymarke <andy.marke@digital.justice.gov.uk>
2025-08-12 12:21:44 +0100

    SDIT-2982 Endpoint to clone court cases to the latest booking
    Linked cases links aka combined cases. Remodelled so JPA is IDs only
    
    TODO - clone the other case children
    * Case status
    * Adjustments

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/LinkCaseTxn.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/LinkCaseTxn.kt
@@ -8,7 +8,6 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.JoinColumns
 import jakarta.persistence.ManyToOne
-import jakarta.persistence.MapsId
 import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 import org.hibernate.Hibernate
