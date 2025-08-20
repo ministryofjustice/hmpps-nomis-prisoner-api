d0d6a4e347b4c84a67cd092ec44ad9b37200fced
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-08-20 17:02:51 +0100

    SDIT-2860 GET scheduled temporary absence

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/movements/TemporaryAbsenceApplicationResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/movements/TemporaryAbsenceApplicationResponse.kt
@@ -17,7 +17,7 @@ data class TemporaryAbsenceApplicationResponse(
   val eventSubType: String,
 
   @Schema(description = "Application date")
-  val applicationDate: LocalDateTime,
+  val applicationDate: LocalDate,
 
   @Schema(description = "From date")
   val fromDate: LocalDate,
