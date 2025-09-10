d08cc015856eab4eae40da871cb0a00e8187748d
karenmillermoj <karen.miller3@justice.gov.uk>
2025-09-10 08:31:08 +0100

    SDIT-3041 Lock row when updating CSIP reports

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/CSIPReportRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/CSIPReportRepository.kt
@@ -1,9 +1,13 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
 
+import jakarta.persistence.LockModeType
+import jakarta.persistence.QueryHint
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.CSIPReport
@@ -53,4 +57,9 @@ interface CSIPReportRepository :
     """,
   )
   fun findIdsByOffenderBookingBookingId(bookingId: Long): List<Long>
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @QueryHints(value = [QueryHint(name = "jakarta.persistence.lock.timeout", value = "2000")])
+  @Query("SELECT c FROM CSIPReport c WHERE c.id = :csipId")
+  fun findByIdOrNullForUpdate(csipId: Long): CSIPReport?
 }
