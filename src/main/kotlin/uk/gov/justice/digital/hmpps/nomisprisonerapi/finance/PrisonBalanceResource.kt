f765d8fe2e5b943070b0ee921f953d4440e7ffba
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-09-16 12:16:38 +0100

    SDIT-3045: ✨ Add in prison ids endpoint (#1387)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonBalanceResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonBalanceResource.kt
@@ -13,16 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.config.ErrorResponse
 import java.math.BigDecimal
+import java.time.LocalDateTime
 
 @RestController
 @Validated
-@RequestMapping("/finance", produces = [MediaType.APPLICATION_JSON_VALUE])
-@PreAuthorize("hasRole('NOMIS_FINANCE')")
+@RequestMapping("/finance/prison", produces = [MediaType.APPLICATION_JSON_VALUE])
+@PreAuthorize("hasRole('NOMIS_PRISONER_API__SYNCHRONISATION__RW')")
 class PrisonBalanceResource {
-  @GetMapping("/prison/{prisonId}/balance")
+  @GetMapping("/ids")
   @Operation(
-    summary = "Gets the balances for the prison.",
-    description = "Retrieves all the balances for a prison. Requires NOMIS_FINANCE role.",
+    summary = "Gets all the caseloads (prisons) that have balance entries.",
+    description = """Retrieves a list of caseloads that have a balance.
+
+      This will include caseloads for prisons that have closed (e.g. Medway STC, Everthorpe) and also caseloads that 
+      aren't prisons (e.g. Transfer, Ghost Holding Establishment).
+
+      Certain caseloads (e.g. CADM_I, NWEB and LIC) don't have a balance so aren't returned.
+
+      Requires NOMIS_PRISONER_API__SYNCHRONISATION__RW role.""",
     responses = [
       ApiResponse(responseCode = "200", description = "Balance information returned."),
       ApiResponse(
@@ -32,7 +40,32 @@ class PrisonBalanceResource {
       ),
       ApiResponse(
         responseCode = "403",
-        description = "Forbidden to access this endpoint. Requires NOMIS_FINANCE",
+        description = "Forbidden to access this endpoint. Requires NOMIS_PRISONER_API__SYNCHRONISATION__RW",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Prison does not exist",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  fun getPrisonIds(): List<String> = listOf("MDI", "LEI")
+
+  @GetMapping("/{prisonId}/balance")
+  @Operation(
+    summary = "Gets the balances for the prison.",
+    description = "Retrieves all the balances for a prison. Requires NOMIS_PRISONER_API__SYNCHRONISATION__RW role.",
+    responses = [
+      ApiResponse(responseCode = "200", description = "Balance information returned."),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden to access this endpoint. Requires NOMIS_PRISONER_API__SYNCHRONISATION__RW",
         content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
       ),
       ApiResponse(
@@ -51,8 +84,8 @@ class PrisonBalanceResource {
     accountBalances = listOf(
       PrisonAccountBalanceDto(
         accountCode = 2101,
-        accountPeriodId = 202604,
         balance = BigDecimal("33.12"),
+        transactionDate = LocalDateTime.now(),
       ),
     ),
   )
@@ -68,8 +101,8 @@ data class PrisonBalanceDto(
 data class PrisonAccountBalanceDto(
   @Schema(description = "The account code for the balance entry", example = "2101")
   val accountCode: Long,
-  @Schema(description = "The most recent accounting period that this entry relates to", example = "202604")
-  val accountPeriodId: Long,
   @Schema(description = "The balance for this prison and account code", example = "33.12")
   val balance: BigDecimal,
+  @Schema(description = "The date and time of the last transaction OR the date and time of the consolidation of the transactions")
+  val transactionDate: LocalDateTime,
 )
--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonBalanceResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonBalanceResourceIntTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.finance
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.helper.builders.NomisDataBuilder
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.integration.IntegrationTestBase
 import uk.gov.justice.hmpps.test.kotlin.auth.WithMockAuthUser
+import java.time.LocalDate
 
 @WithMockAuthUser
 class PrisonBalanceResourceIntTest : IntegrationTestBase() {
@@ -23,6 +25,49 @@ class PrisonBalanceResourceIntTest : IntegrationTestBase() {
 
   @AfterEach
   fun tearDown() {
+  }
+
+  @Nested
+  @DisplayName("GET /finance/prison/ids")
+  inner class PrisonIdsTests {
+    @Nested
+    inner class Security {
+      @Test
+      fun `access forbidden when no role`() {
+        webTestClient.get().uri("/finance/prison/ids")
+          .headers(setAuthorisation(roles = listOf()))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access forbidden with wrong role`() {
+        webTestClient.get().uri("/finance/prison/ids")
+          .headers(setAuthorisation(roles = listOf("ROLE_BANANAS")))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access unauthorised with no auth token`() {
+        webTestClient.get().uri("/finance/prison/ids")
+          .exchange()
+          .expectStatus().isUnauthorized
+      }
+    }
+
+    @Test
+    fun getPrisonBalance() {
+      webTestClient.get().uri("/finance/prison/ids")
+        .headers(setAuthorisation(roles = listOf("ROLE_NOMIS_PRISONER_API__SYNCHRONISATION__RW")))
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody()
+        .jsonPath("$[0]").isEqualTo("MDI")
+        .jsonPath("$[1]").isEqualTo("LEI")
+        .jsonPath("$.length()").isEqualTo(2)
+    }
   }
 
   @Nested
@@ -57,22 +102,25 @@ class PrisonBalanceResourceIntTest : IntegrationTestBase() {
     @Test
     fun getPrisonBalance() {
       webTestClient.get().uri("/finance/prison/MDI/balance")
-        .headers(setAuthorisation(roles = listOf("ROLE_NOMIS_FINANCE")))
+        .headers(setAuthorisation(roles = listOf("ROLE_NOMIS_PRISONER_API__SYNCHRONISATION__RW")))
         .exchange()
         .expectStatus()
         .isOk
         .expectBody()
         .jsonPath("$.prisonId").isEqualTo("MDI")
         .jsonPath("$.accountBalances[0].accountCode").isEqualTo(2101)
-        .jsonPath("$.accountBalances[0].accountPeriodId").isEqualTo(202604)
         .jsonPath("$.accountBalances[0].balance").isEqualTo("33.12")
+        .jsonPath("$.accountBalances[0].transactionDate").value<String> {
+          assertThat(it).startsWith(LocalDate.now().toString())
+        }
+        .jsonPath("$.accountBalances.length()").isEqualTo(1)
     }
 
     @Test
     @Disabled
     fun `none found`() {
       webTestClient.get().uri("/finance/prison/XXX/balance")
-        .headers(setAuthorisation(roles = listOf("ROLE_NOMIS_FINANCE")))
+        .headers(setAuthorisation(roles = listOf("ROLE_NOMIS_PRISONER_API__SYNCHRONISATION__RW")))
         .exchange()
         .expectStatus()
         .isNotFound
