f441bd6311b2dcb94badcf8015fe7b422ab0df4c
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-09-17 13:48:57 +0100

    SDIT-3045: ✅ Improve prison balance tests (#1392)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonBalanceResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonBalanceResourceIntTest.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.finance
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -8,29 +7,39 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.helper.builders.NomisDataBuilder
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.helper.builders.Repository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.integration.IntegrationTestBase
 import uk.gov.justice.hmpps.test.kotlin.auth.WithMockAuthUser
 import java.math.BigDecimal
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 @WithMockAuthUser
 class PrisonBalanceResourceIntTest : IntegrationTestBase() {
   @Autowired
   private lateinit var nomisDataBuilder: NomisDataBuilder
 
+  @Autowired
+  private lateinit var repository: Repository
+
   @BeforeEach
   fun setUp() {
     nomisDataBuilder.build {
       caseloadCurrentAccountBase(caseloadId = "LEI", currentBalance = BigDecimal("23.45")) {
-        transaction(currentBalance = BigDecimal("12.23"))
-        transaction(currentBalance = BigDecimal("11.22"))
+        transaction(currentBalance = BigDecimal("12.23"), createDateTime = LocalDateTime.parse("2019-09-09T04:03:02"))
+        transaction(currentBalance = BigDecimal("11.22"), createDateTime = LocalDateTime.parse("2020-09-09T04:03:02"))
       }
-      caseloadCurrentAccountBase(caseloadId = "MDI", currentBalance = BigDecimal("5.67"))
+      caseloadCurrentAccountBase(caseloadId = "LEI", currentBalance = BigDecimal("5.67"), accountCode = 2102) {
+        transaction(currentBalance = BigDecimal("9.12"), createDateTime = LocalDateTime.parse("2020-10-09T04:03:02"))
+      }
+      caseloadCurrentAccountBase(caseloadId = "MDI", currentBalance = BigDecimal("5.67")) {
+        transaction(currentBalance = BigDecimal("12.23"), createDateTime = LocalDateTime.parse("2019-09-09T04:03:02"))
+      }
     }
   }
 
   @AfterEach
   fun tearDown() {
+    repository.deleteAllPrisonBalances()
   }
 
   @Nested
@@ -106,7 +115,7 @@ class PrisonBalanceResourceIntTest : IntegrationTestBase() {
     }
 
     @Test
-    fun getPrisonBalance() {
+    fun getPrisonBalanceLeeds() {
       webTestClient.get().uri("/finance/prison/LEI/balance")
         .headers(setAuthorisation(roles = listOf("ROLE_NOMIS_PRISONER_API__SYNCHRONISATION__RW")))
         .exchange()
@@ -116,9 +125,25 @@ class PrisonBalanceResourceIntTest : IntegrationTestBase() {
         .jsonPath("$.prisonId").isEqualTo("LEI")
         .jsonPath("$.accountBalances[0].accountCode").isEqualTo(2101)
         .jsonPath("$.accountBalances[0].balance").isEqualTo("23.45")
-        .jsonPath("$.accountBalances[0].transactionDate").value<String> {
-          assertThat(it).startsWith(LocalDate.now().toString())
-        }
+        .jsonPath("$.accountBalances[0].transactionDate").isEqualTo("2020-09-09T04:03:02")
+        .jsonPath("$.accountBalances[1].accountCode").isEqualTo(2102)
+        .jsonPath("$.accountBalances[1].balance").isEqualTo("9.12")
+        .jsonPath("$.accountBalances[1].transactionDate").isEqualTo("2020-10-09T04:03:02")
+        .jsonPath("$.accountBalances.length()").isEqualTo(2)
+    }
+
+    @Test
+    fun getPrisonBalanceMoorland() {
+      webTestClient.get().uri("/finance/prison/MDI/balance")
+        .headers(setAuthorisation(roles = listOf("ROLE_NOMIS_PRISONER_API__SYNCHRONISATION__RW")))
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody()
+        .jsonPath("$.prisonId").isEqualTo("MDI")
+        .jsonPath("$.accountBalances[0].accountCode").isEqualTo(2101)
+        .jsonPath("$.accountBalances[0].balance").isEqualTo("12.23")
+        .jsonPath("$.accountBalances[0].transactionDate").isEqualTo("2019-09-09T04:03:02")
         .jsonPath("$.accountBalances.length()").isEqualTo(1)
     }
 
