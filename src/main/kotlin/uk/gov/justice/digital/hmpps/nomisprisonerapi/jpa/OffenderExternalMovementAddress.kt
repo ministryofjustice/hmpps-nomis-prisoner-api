a1ece0351bbe6743bdc58de230e0bf00d356898b
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-10-13 15:56:52 +0100

    SDIT-2856 Make external movement address a value object the same as V_ADDRESSES (#1433)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/OffenderExternalMovementAddress.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Inheritance
+import jakarta.persistence.Table
+import org.hibernate.Hibernate
+
+@Entity
+@Table(name = "V_ADDRESSES")
+@Inheritance
+class OffenderExternalMovementAddress(
+  @Id
+  val addressId: Long,
+
+  val ownerClass: String,
+
+  val commentText: String? = null,
+
+  val house: String? = null,
+
+  val street: String? = null,
+
+  val locality: String? = null,
+
+  val cityName: String? = null,
+
+  val county: String? = null,
+
+  val country: String? = null,
+
+  val postalCode: String? = null,
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+    other as OffenderExternalMovementAddress
+    return addressId == other.addressId
+  }
+
+  override fun hashCode(): Int = javaClass.hashCode()
+}
