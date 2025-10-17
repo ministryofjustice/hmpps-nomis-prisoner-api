e38b497e9d214e860dc166eec4c28c953b18c0ed
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2025-10-17 15:43:58 +0100

    SDIT-3082 Prisoner balance reconciliation (#1441)
    * SDIT-3082 Prisoner balance reconciliation
    
    Add offenderId to GET prisoner endpoint
    
    * SDIT-3082 Prisoner balance reconciliation
    
    Add offenderId to GET prisoner endpoint
    
    ---------
    
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/prisoners/PrisonerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/prisoners/PrisonerService.kt
@@ -82,8 +82,10 @@ class PrisonerService(
 
 private fun OffenderBooking.toPrisonerDetails(): PrisonerDetails = PrisonerDetails(
   offenderNo = offender.nomsId,
+  offenderId = offender.id,
+  rootOffenderId = offender.rootOffenderId,
   bookingId = bookingId,
-  location = location?.id ?: "",
+  location = location.id,
   active = active,
 )
 
