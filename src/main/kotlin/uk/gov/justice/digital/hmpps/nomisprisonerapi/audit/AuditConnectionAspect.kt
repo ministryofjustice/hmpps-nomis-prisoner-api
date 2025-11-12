59ff5f473066ccaa0191066bc44177bbba2bf78e
karenmillermoj <karen.miller3@justice.gov.uk>
2025-11-12 09:35:30 +0000

    SDIT-3155 Improve code readability

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/audit/AuditConnectionAspect.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/audit/AuditConnectionAspect.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.nomisprisonerapi.audit
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
-import org.slf4j.LoggerFactory
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.jdbc.datasource.DataSourceUtils
@@ -17,15 +16,13 @@ class AuditConnectionAspect(
   private val auditHandler: AuditConnectionHandler,
   private val dataSource: DataSource,
 ) {
-  companion object {
-    private val log = LoggerFactory.getLogger(this::class.java)
-  }
-
   @Around(
-    "@within(org.springframework.transaction.annotation.Transactional) || " +
-      "@annotation(org.springframework.transaction.annotation.Transactional) || " +
-      "@within(jakarta.transaction.Transactional) || " +
-      "@annotation(jakarta.transaction.Transactional)",
+    """
+      @within(org.springframework.transaction.annotation.Transactional) ||  
+      @annotation(org.springframework.transaction.annotation.Transactional) || 
+      @within(jakarta.transaction.Transactional) || 
+      @annotation(jakarta.transaction.Transactional)
+      """,
   )
   fun applyAuditModuleAround(joinPoint: ProceedingJoinPoint): Any? {
     val conn = DataSourceUtils.getConnection(dataSource)
