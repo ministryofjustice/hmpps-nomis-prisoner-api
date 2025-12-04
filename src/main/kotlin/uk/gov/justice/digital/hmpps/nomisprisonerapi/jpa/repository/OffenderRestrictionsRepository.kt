f9454a10fc24aa849483858dc0dcc07073e85e44
andymarke <andy.marke@digital.justice.gov.uk>
2025-12-04 10:41:39 +0000

    SDIT-3246 Removed unused person and restriction ids

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderRestrictionsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderRestrictionsRepository.kt
@@ -1,41 +1,12 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
 
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderRestrictions
-import java.time.LocalDateTime
 
 @Repository
 interface OffenderRestrictionsRepository : JpaRepository<OffenderRestrictions, Long> {
-  @Query(
-    """
-      select 
-        r.id as restrictionId
-      from OffenderRestrictions r 
-    """,
-  )
-  fun findAllIds(
-    pageable: Pageable,
-  ): Page<RestrictionIdProjection>
-
-  @Query(
-    """
-      select 
-        r.id as restrictionId
-      from OffenderRestrictions r 
-        where 
-          (:fromDate is null or r.createDatetime >= :fromDate) and 
-          (:toDate is null or r.createDatetime <= :toDate) 
-    """,
-  )
-  fun findAllIds(
-    fromDate: LocalDateTime?,
-    toDate: LocalDateTime?,
-    pageable: Pageable,
-  ): Page<RestrictionIdProjection>
 
   @Query(
     """
@@ -49,8 +20,4 @@ interface OffenderRestrictionsRepository : JpaRepository<OffenderRestrictions, L
     nativeQuery = true,
   )
   fun findAllIdsFromId(restrictionId: Long, pageSize: Int): List<Long>
-}
-
-interface RestrictionIdProjection {
-  val restrictionId: Long
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/PersonRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/PersonRepository.kt
@@ -1,44 +1,15 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
 
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.Person
-import java.time.LocalDateTime
 
 @Repository
 interface PersonRepository :
   CrudRepository<Person, Long>,
   JpaRepository<Person, Long> {
-  @Query(
-    """
-      select 
-        p.id as personId
-      from Person p 
-    """,
-  )
-  fun findAllPersonIds(
-    pageable: Pageable,
-  ): Page<PersonIdProjection>
-
-  @Query(
-    """
-      select 
-        p.id as personId
-      from Person p 
-        where 
-          (:fromDate is null or p.createDatetime >= :fromDate) and 
-          (:toDate is null or p.createDatetime <= :toDate) 
-    """,
-  )
-  fun findAllPersonIds(
-    fromDate: LocalDateTime?,
-    toDate: LocalDateTime?,
-    pageable: Pageable,
-  ): Page<PersonIdProjection>
 
   @Query(
     """
@@ -52,8 +23,4 @@ interface PersonRepository :
     nativeQuery = true,
   )
   fun findAllIdsFromId(personId: Long, pageSize: Int): List<Long>
-}
-
-interface PersonIdProjection {
-  val personId: Long
 }
