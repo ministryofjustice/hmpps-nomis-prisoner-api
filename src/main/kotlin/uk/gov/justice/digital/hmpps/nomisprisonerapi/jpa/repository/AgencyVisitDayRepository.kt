cf41a667b815e536cd296aa45aae3c6563a35b5d
Andy Marke <andy.marke@digital.justice.gov.uk>
2026-02-05 12:01:57 +0000

    SDIT-3438 Added GET /visits/configuration/prisons (#1582)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/AgencyVisitDayRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/AgencyVisitDayRepository.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.AgencyVisitDay
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.AgencyVisitDayId
@@ -13,4 +14,7 @@ interface AgencyVisitDayRepository : JpaRepository<AgencyVisitDay, AgencyVisitDa
     weekDay: WeekDay,
     locationId: String,
   ): AgencyVisitDay?
+
+  @Query("select distinct avd.agencyVisitDayId.location.id from AgencyVisitDay avd join AgencyLocation al on al = avd.agencyVisitDayId.location where al.deactivationDate is null")
+  fun findDistinctPrisonIdByActivePrisons(): List<String>
 }
