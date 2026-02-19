b847bef3cbded174a8e5d3ab6239d64acecafa51
Mike Halma <mike.halma@digital.justice.gov.uk>
2026-02-19 14:52:14 +0000

    SDIT-3485 Handle more falvours of bad TAP data
    Handles TAP schedules without an application and TAP movements linked to a non-TAP schedule, neither of which is possible these days

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderExternalMovementRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderExternalMovementRepository.kt
@@ -16,10 +16,16 @@ interface OffenderExternalMovementRepository : CrudRepository<OffenderExternalMo
         join OFFENDER_BOOKINGS ob on o.offender_id=ob.root_offender_id
         join OFFENDER_EXTERNAL_MOVEMENTS oem on ob.OFFENDER_BOOK_ID=oem.OFFENDER_BOOK_ID
         left join OFFENDER_IND_SCHEDULES ois on oem.EVENT_ID=ois.EVENT_ID
+        left join OFFENDER_MOVEMENT_APPS oma on ois.OFFENDER_MOVEMENT_APP_ID=oma.OFFENDER_MOVEMENT_APP_ID
       where oem.MOVEMENT_TYPE='TAP' 
         and o.OFFENDER_ID_DISPLAY=:offender 
         and oem.DIRECTION_CODE = 'OUT' 
-        and (oem.event_id is null or ois.EVENT_ID is null)
+        and (
+          oem.event_id is null 
+          or ois.EVENT_ID is null 
+          or ois.EVENT_TYPE != 'TAP'
+          or oma.OFFENDER_MOVEMENT_APP_ID is null
+        )
     """,
     nativeQuery = true,
   )
@@ -33,10 +39,19 @@ interface OffenderExternalMovementRepository : CrudRepository<OffenderExternalMo
              join OFFENDER_EXTERNAL_MOVEMENTS oem on ob.OFFENDER_BOOK_ID=oem.OFFENDER_BOOK_ID
              left join OFFENDER_IND_SCHEDULES ois_in on oem.EVENT_ID=ois_in.EVENT_ID
              left join OFFENDER_IND_SCHEDULES ois_out on oem.PARENT_EVENT_ID=ois_out.EVENT_ID
+             left join offender_movement_apps oma on ois_out.OFFENDER_MOVEMENT_APP_ID=oma.OFFENDER_MOVEMENT_APP_ID
       where oem.MOVEMENT_TYPE='TAP'
         and o.OFFENDER_ID_DISPLAY=:offender
         and oem.DIRECTION_CODE = 'IN'
-        and (ois_in.EVENT_ID is null or ois_out.EVENT_ID is null or ois_in.PARENT_EVENT_ID != ois_out.EVENT_ID)
+        and (
+          ois_in.EVENT_ID is null 
+          or ois_in.EVENT_TYPE != 'TAP'
+          or ois_out.EVENT_ID is null 
+          or ois_out.EVENT_TYPE != 'TAP'
+          or ois_in.PARENT_EVENT_ID != ois_out.EVENT_ID 
+          or ois_in.PARENT_EVENT_ID != ois_out.EVENT_ID 
+          or oma.OFFENDER_MOVEMENT_APP_ID is null
+        )
     """,
     nativeQuery = true,
   )
@@ -50,8 +65,11 @@ interface OffenderExternalMovementRepository : CrudRepository<OffenderExternalMo
         join OFFENDER_EXTERNAL_MOVEMENTS oem on ob.OFFENDER_BOOK_ID=oem.OFFENDER_BOOK_ID
         -- the scheduled OUT movement must exist to be included - some have been deleted
         join OFFENDER_IND_SCHEDULES ois on oem.EVENT_ID=ois.EVENT_ID
-      where oem.MOVEMENT_TYPE='TAP' and o.OFFENDER_ID_DISPLAY=:offender and oem.DIRECTION_CODE = 'OUT'
-        and ois.OFFENDER_MOVEMENT_APP_ID is not null
+        join OFFENDER_MOVEMENT_APPS oma on ois.OFFENDER_MOVEMENT_APP_ID=oma.OFFENDER_MOVEMENT_APP_ID
+      where oem.MOVEMENT_TYPE='TAP' 
+        and o.OFFENDER_ID_DISPLAY=:offender 
+        and oem.DIRECTION_CODE = 'OUT'
+        and ois.EVENT_TYPE = 'TAP'
     """,
     nativeQuery = true,
   )
@@ -65,11 +83,13 @@ interface OffenderExternalMovementRepository : CrudRepository<OffenderExternalMo
              join OFFENDER_EXTERNAL_MOVEMENTS oem on ob.OFFENDER_BOOK_ID=oem.OFFENDER_BOOK_ID
              join OFFENDER_IND_SCHEDULES ois_in on oem.EVENT_ID=ois_in.EVENT_ID
              join OFFENDER_IND_SCHEDULES ois_out on oem.PARENT_EVENT_ID=ois_out.EVENT_ID
+             join OFFENDER_MOVEMENT_APPS oma on ois_out.OFFENDER_MOVEMENT_APP_ID=oma.OFFENDER_MOVEMENT_APP_ID
       where oem.MOVEMENT_TYPE='TAP'
         and o.OFFENDER_ID_DISPLAY=:offender
         and oem.DIRECTION_CODE = 'IN'
         and ois_out.EVENT_ID = ois_in.PARENT_EVENT_ID
-        and ois_out.OFFENDER_MOVEMENT_APP_ID is not null
+        and ois_out.EVENT_TYPE = 'TAP'
+        and ois_in.EVENT_TYPE = 'TAP'
     """,
     nativeQuery = true,
   )
