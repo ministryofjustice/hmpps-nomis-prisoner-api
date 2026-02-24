3b9fb6660b63757fc7df632f990251d5bfd97c61
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2026-02-24 11:31:00 +0000

    SDIT-3314 Add endpoint to get offender transaction from last id (#1621)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonerTransactionsResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonerTransactionsResource.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.finance
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -15,6 +17,11 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.config.ErrorResponse
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.PostingType
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.SubAccountType
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @RestController
 @Validated
@@ -53,6 +60,47 @@ class PrisonerTransactionsResource(
     transactionId: Long,
   ): List<OffenderTransactionDto> = transactionsService.getOffenderTransactions(transactionId)
 
+  @GetMapping("/transactions/from/{transactionId}")
+  @ResponseStatus(HttpStatus.OK)
+  @Operation(
+    summary = "Get a page of prisoner transaction Ids starting from an id onwards",
+    description = """Retrieves transactions to be iterated over using last transaction Id and entry date.
+      Requires NOMIS_PRISONER_API__SYNCHRONISATION__RW""",
+    responses = [
+      ApiResponse(responseCode = "200", description = "Transaction Information Returned"),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden to access this endpoint. Requires NOMIS_PRISONER_API__SYNCHRONISATION__RW",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Transaction does not exist",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  fun findPrisonerTransactionsFromId(
+    @Schema(description = "If supplied get prisoner transaction starting after this id", example = "1555999")
+    @PathVariable(value = "transactionId")
+    transactionId: Long,
+    @Schema(description = "Number of prisoner transaction ids to get", required = false, defaultValue = "20")
+    @RequestParam(value = "size", defaultValue = "20")
+    size: Int,
+    @RequestParam(value = "entryDate")
+    @Parameter(description = "Filter results by transactions that were created on the given date", required = false, example = "2024-11-03")
+    entryDate: LocalDate = LocalDate.now(),
+  ): PrisonerTransactionIdsPage = transactionsService.getPrisonerTransactionIds(
+    transactionId = transactionId,
+    pageSize = size,
+    entryDate = entryDate,
+  )
+
   @GetMapping("/transactions/from/{transactionId}/{transactionEntrySequence}")
   @ResponseStatus(HttpStatus.OK)
   @Operation(
@@ -77,7 +125,7 @@ class PrisonerTransactionsResource(
       ),
     ],
   )
-  fun findOffenderTransactionsFromId(
+  fun findOffenderTransactionsFromIdAndSequence(
     @Schema(description = "Id of last transaction before intended start", example = "123456789")
     @PathVariable
     transactionId: Long,
@@ -101,4 +149,72 @@ class PrisonerTransactionsResource(
     )
 }
 
-enum class SourceSystem { DPS, NOMIS }
+@Schema(description = "The data held in NOMIS about a financial transaction")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class OffenderTransactionDto(
+  @Schema(description = "The transaction id", example = "12345678")
+  val transactionId: Long,
+
+  @Schema(description = "The sequence number", example = "3")
+  val transactionEntrySequence: Int,
+
+  @Schema(description = "The offender id", example = "1234567")
+  val offenderId: Long,
+
+  @Schema(description = "The prisoner number or noms id", example = "A1234AA")
+  val offenderNo: String,
+
+  @Schema(description = "The booking id", example = "12345678")
+  val bookingId: Long?,
+
+  @Schema(description = "The caseload", example = "MDI")
+  val caseloadId: String,
+
+  @Schema(description = "The transaction amount", example = "2.14")
+  val amount: BigDecimal,
+
+  @Schema(description = "The transaction type defined in the TRANSACTION_TYPES table", example = "CANT")
+  val type: String,
+
+  @Schema(description = "Whether credit or debit")
+  val postingType: PostingType,
+
+  @Schema(description = "A description of the transaction entry", example = "Television")
+  val description: String,
+
+  @Schema(description = "The transaction date", example = "2025-06-14")
+  val entryDate: LocalDate,
+
+  @Schema(description = "The client unique reference", example = "MKI6431RETAILRECEIPTPHONE")
+  val clientReference: String?,
+
+  @Schema(description = "The transaction reference", example = "1423558449")
+  val reference: String?,
+
+  @Schema(description = "The account type")
+  val subAccountType: SubAccountType,
+
+  @Schema(description = "Dependent GL transaction entries")
+  val generalLedgerTransactions: List<GeneralLedgerTransactionDto>,
+
+  val createdAt: LocalDateTime,
+  val createdBy: String,
+  val createdByDisplayName: String,
+
+  val lastModifiedAt: LocalDateTime? = null,
+  val lastModifiedBy: String? = null,
+  val lastModifiedByDisplayName: String? = null,
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class PrisonerTransactionIdsPage(
+  @Schema(description = "Page of prisoner Transaction Ids")
+  val ids: List<PrisonerTransactionIdResponse>,
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "Prisoner transaction id")
+data class PrisonerTransactionIdResponse(
+  @Schema(description = "The prisoner transaction id", required = true)
+  val transactionId: Long,
+)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/TransactionsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/TransactionsService.kt
@@ -1,21 +1,15 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.finance
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.data.domain.Limit
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.data.NotFoundException
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.GeneralLedgerTransaction
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderTransaction
-import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.PostingType
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.StaffUserAccount
-import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.SubAccountType
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.GeneralLedgerTransactionRepository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository.OffenderTransactionRepository
-import java.math.BigDecimal
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 @Service
 @Transactional
@@ -23,10 +17,6 @@ class TransactionsService(
   val offenderTransactionRepository: OffenderTransactionRepository,
   val generalLedgerTransactionRepository: GeneralLedgerTransactionRepository,
 ) {
-  fun getOffenderTransactions(transactionId: Long): List<OffenderTransactionDto> = offenderTransactionRepository
-    .findByTransactionId(transactionId)
-    .map(::mapOT)
-
   fun getGeneralLedgerTransactions(
     transactionId: Long,
   ): List<GeneralLedgerTransactionDto> = generalLedgerTransactionRepository
@@ -53,6 +43,10 @@ class TransactionsService(
     .findByTransactionId(transactionId)
     .map(::mapGL)
 
+  fun getOffenderTransactions(transactionId: Long): List<OffenderTransactionDto> = offenderTransactionRepository
+    .findByTransactionId(transactionId)
+    .map(::mapOT)
+
   fun findOrphanTransactionsFromId(
     transactionId: Long,
     transactionEntrySequence: Int,
@@ -78,6 +72,17 @@ class TransactionsService(
       .findByTransactionIdGreaterThan(transactionId, transactionEntrySequence, Limit.of(pageSize))
     return data.map(::mapOT)
   }
+
+  fun getPrisonerTransactionIds(
+    transactionId: Long,
+    pageSize: Int,
+    entryDate: LocalDate,
+  ): PrisonerTransactionIdsPage = offenderTransactionRepository.findAllPrisonerTransactionIdsWithDateFilter(
+    entryDate = entryDate,
+    prisonerTransactionId = transactionId,
+    pageSize = pageSize,
+  )
+    .map { PrisonerTransactionIdResponse(transactionId = it.id) }.let { PrisonerTransactionIdsPage(it) }
 }
 
 private fun mapOT(transaction: OffenderTransaction): OffenderTransactionDto = OffenderTransactionDto(
@@ -126,105 +131,3 @@ private fun mapGL(gl: GeneralLedgerTransaction): GeneralLedgerTransactionDto = G
 
 private fun getDisplayName(staffUserAccount: StaffUserAccount?): String = staffUserAccount
   ?.staff?.run { "$firstName $lastName" } ?: "Unknown"
-
-@Schema(description = "The data held in NOMIS about a financial transaction")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderTransactionDto(
-  @Schema(description = "The transaction id", example = "12345678")
-  val transactionId: Long,
-
-  @Schema(description = "The sequence number", example = "3")
-  val transactionEntrySequence: Int,
-
-  @Schema(description = "The offender id", example = "1234567")
-  val offenderId: Long,
-
-  @Schema(description = "The prisoner number or noms id", example = "A1234AA")
-  val offenderNo: String,
-
-  @Schema(description = "The booking id", example = "12345678")
-  val bookingId: Long?,
-
-  @Schema(description = "The caseload", example = "MDI")
-  val caseloadId: String,
-
-  @Schema(description = "The transaction amount", example = "2.14")
-  val amount: BigDecimal,
-
-  @Schema(description = "The transaction type defined in the TRANSACTION_TYPES table", example = "CANT")
-  val type: String,
-
-  @Schema(description = "Whether credit or debit")
-  val postingType: PostingType,
-
-  @Schema(description = "A description of the transaction entry", example = "Television")
-  val description: String,
-
-  @Schema(description = "The transaction date", example = "2025-06-14")
-  val entryDate: LocalDate,
-
-  @Schema(description = "The client unique reference", example = "MKI6431RETAILRECEIPTPHONE")
-  val clientReference: String?,
-
-  @Schema(description = "The transaction reference", example = "1423558449")
-  val reference: String?,
-
-  @Schema(description = "The account type")
-  val subAccountType: SubAccountType,
-
-  @Schema(description = "Dependent GL transaction entries")
-  val generalLedgerTransactions: List<GeneralLedgerTransactionDto>,
-
-  val createdAt: LocalDateTime,
-  val createdBy: String,
-  val createdByDisplayName: String,
-
-  val lastModifiedAt: LocalDateTime? = null,
-  val lastModifiedBy: String? = null,
-  val lastModifiedByDisplayName: String? = null,
-)
-
-@Schema(description = "The data held in NOMIS about a general ledger transaction")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class GeneralLedgerTransactionDto(
-  @Schema(description = "The transaction id", example = "12345678")
-  val transactionId: Long,
-
-  @Schema(description = "The sequence number", example = "3")
-  val transactionEntrySequence: Int,
-
-  @Schema(description = "The GL entry number", example = "2")
-  val generalLedgerEntrySequence: Int,
-
-  @Schema(description = "The caseload", example = "MDI")
-  val caseloadId: String,
-
-  @Schema(description = "The transaction amount", example = "2.14")
-  val amount: BigDecimal,
-
-  @Schema(description = "The transaction type defined in the TRANSACTION_TYPES table", example = "CANT")
-  val type: String,
-
-  @Schema(description = "Whether credit or debit")
-  val postingType: PostingType,
-
-  @Schema(description = "The account code", example = "21020")
-  val accountCode: Int,
-
-  @Schema(description = "A description of the transaction entry", example = "???")
-  val description: String,
-
-  @Schema(description = "When the transaction occurred", example = "2025-07-14T12:13:14")
-  val transactionTimestamp: LocalDateTime,
-
-  @Schema(description = "The transaction reference", example = "1423558449")
-  val reference: String?,
-
-  val createdAt: LocalDateTime,
-  val createdBy: String,
-  val createdByDisplayName: String,
-
-  val lastModifiedAt: LocalDateTime? = null,
-  val lastModifiedBy: String? = null,
-  val lastModifiedByDisplayName: String? = null,
-)
