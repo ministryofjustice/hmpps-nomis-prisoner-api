532919d48c9071408d425ae541c401b1d8829369
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-04-30 16:55:16 +0100

    SDIT-3723 Design API for court movements (#1735)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/movements/court/movement/CourtMovementModel.kt
@@ -0,0 +1,72 @@
+package uk.gov.justice.digital.hmpps.nomisprisonerapi.movements.court.movement
+
+import io.swagger.v3.oas.annotations.media.Schema
+import uk.gov.justice.digital.hmpps.nomisprisonerapi.helpers.NomisAudit
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Schema(description = "Court Movement Out")
+data class CourtMovementOut(
+  @Schema(description = "Booking ID")
+  val bookingId: Long,
+
+  @Schema(description = "Movement sequence")
+  val sequence: Int,
+
+  @Schema(description = "Court schedule out event ID. Empty for unscheduled movements.")
+  val courtScheduleOutId: Long?,
+
+  @Schema(description = "Movement date")
+  val movementDate: LocalDate,
+
+  @Schema(description = "Movement time")
+  val movementTime: LocalDateTime,
+
+  @Schema(description = "Movement reason")
+  val movementReason: String,
+
+  @Schema(description = "From prison")
+  val fromPrison: String,
+
+  @Schema(description = "To agency")
+  val toAgency: String?,
+
+  @Schema(description = "Comment text")
+  val commentText: String?,
+
+  @Schema(description = "Audit data associated with the records")
+  val audit: NomisAudit,
+)
+
+@Schema(description = "Court Movement In")
+data class CourtMovementIn(
+  @Schema(description = "Booking ID")
+  val bookingId: Long,
+
+  @Schema(description = "Movement sequence")
+  val sequence: Int,
+
+  @Schema(description = "Court schedule out event ID. Empty for unscheduled movements.")
+  val courtScheduleOutId: Long?,
+
+  @Schema(description = "Movement date")
+  val movementDate: LocalDate,
+
+  @Schema(description = "Movement time")
+  val movementTime: LocalDateTime,
+
+  @Schema(description = "Movement reason")
+  val movementReason: String,
+
+  @Schema(description = "From agency")
+  val fromAgency: String,
+
+  @Schema(description = "To prison")
+  val toPrison: String?,
+
+  @Schema(description = "Comment text")
+  val commentText: String?,
+
+  @Schema(description = "Audit data associated with the records")
+  val audit: NomisAudit,
+)
