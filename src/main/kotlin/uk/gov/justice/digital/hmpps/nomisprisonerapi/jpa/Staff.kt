a7e623c7b57b6697df81f2b752482a8ab850417e
karenmillermoj <karen.miller3@justice.gov.uk>
2026-05-21 11:34:48 +0100

    SDIT-3808 formulas always loaded

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/Staff.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/Staff.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
@@ -32,7 +31,7 @@ class Staff(
   @Column
   val lastName: String,
 
-  @ManyToOne(fetch = FetchType.LAZY)
+  @ManyToOne
   @JoinColumnsOrFormulas(
     value = [
       JoinColumnOrFormula(
