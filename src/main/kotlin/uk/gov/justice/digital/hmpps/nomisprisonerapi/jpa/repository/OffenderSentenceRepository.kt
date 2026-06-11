b7656d57972db61a64992589ed694b97516260bf
andymarke <andy.marke@justice.gov.uk>
2026-06-11 14:38:06 +0100

    SDIT-3888 Recall sentence lock
    When loading sentence to update wait for lock to prevent hanging

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderSentenceRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderSentenceRepository.kt
@@ -1,8 +1,12 @@
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
 
+import jakarta.persistence.LockModeType
+import jakarta.persistence.QueryHint
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderBooking
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderSentence
@@ -13,8 +17,6 @@ interface OffenderSentenceRepository :
   JpaRepository<OffenderSentence, SentenceId>,
   JpaSpecificationExecutor<OffenderSentence> {
 
-  fun deleteByIdOffenderBookingBookingId(bookingId: Long)
-
   fun findByIdAndCourtCaseId(sentenceId: SentenceId, courtCaseId: Long): OffenderSentence?
 
   @Query("select coalesce(max(id.sequence), 0) + 1 from OffenderSentence where id.offenderBooking = :offenderBooking")
@@ -24,4 +26,9 @@ interface OffenderSentenceRepository :
   fun getNextLineSequence(offenderBooking: OffenderBooking): Long
 
   fun findByIdOffenderBookingBookingIdInAndAuditModuleNameAndStatusAndCourtCaseIsNotNull(offenderBookingIds: List<Long>, auditModule: String = "MERGE", status: String = "I"): List<OffenderSentence>
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @QueryHints(value = [QueryHint(name = "jakarta.persistence.lock.timeout", value = "2000")])
+  @Query("SELECT s FROM OffenderSentence s WHERE s.id = :id")
+  fun findByIdOrNullForUpdate(id: SentenceId): OffenderSentence?
 }
