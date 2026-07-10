c7f914475bae23d0b6fb97dbfc5543e688c8f2fb
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-07-10 17:04:05 +0100

    SDIT-4032 Handle race to delete schedule (#1879)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderTapScheduleOutRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/jpa/repository/OffenderTapScheduleOutRepository.kt
@@ -2,7 +2,10 @@
 
 package uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.repository
 
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.jpa.OffenderTapScheduleOut
 
@@ -11,4 +14,8 @@ interface OffenderTapScheduleOutRepository : JpaRepository<OffenderTapScheduleOu
   fun findByEventIdAndOffenderBooking_Offender_NomsId(eventId: Long, offenderNo: String): OffenderTapScheduleOut?
 
   fun countByOffenderBooking_Offender_NomsId_AndTapApplicationIsNotNull(offenderNo: String): Long
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query(value = "select tso from OffenderTapScheduleOut tso where (tso.eventId = :eventId)")
+  fun findByEventIdOrNullForUpdate(eventId: Long): OffenderTapScheduleOut?
 }
