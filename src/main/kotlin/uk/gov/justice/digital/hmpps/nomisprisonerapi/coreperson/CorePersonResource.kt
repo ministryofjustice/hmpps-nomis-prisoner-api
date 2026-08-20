fc8dd22fcbeb9851ec691d1d282933f0250783d7
Richard Popple <2195350+richardpopple@users.noreply.github.com>
2026-08-20 14:42:53 +0100

    SDIT-4164 removing currently unused fields

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/coreperson/CorePersonResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/coreperson/CorePersonResource.kt
@@ -21,7 +21,6 @@ import uk.gov.justice.digital.hmpps.nomisprisonerapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.data.CodeDescription
 import uk.gov.justice.digital.hmpps.nomisprisonerapi.helpers.NomisAudit
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 @RestController
 @Validated
@@ -327,24 +326,6 @@ data class CorePerson(
   val activeFlag: Boolean,
   @Schema(description = "List of offender records for the person")
   val offenders: List<CoreOffender>? = emptyList(),
-  @Schema(description = "List of distinct sentence start dates")
-  val sentenceStartDates: List<LocalDate>? = emptyList(),
-  @Schema(description = "List of addresses for the person")
-  val addresses: List<OffenderAddress>? = emptyList(),
-  @Schema(description = "List of phone numbers for the person")
-  val phoneNumbers: List<OffenderPhoneNumber>? = emptyList(),
-  @Schema(description = "List of email addresses for the person")
-  val emailAddresses: List<OffenderEmailAddress>? = emptyList(),
-  @Schema(description = "List of nationalities for the person")
-  val nationalities: List<OffenderNationality>? = emptyList(),
-  @Schema(description = "List of nationality details for the person")
-  val nationalityDetails: List<OffenderNationalityDetails>? = emptyList(),
-  @Schema(description = "List of sexual orientations for the person")
-  val sexualOrientations: List<OffenderSexualOrientation>? = emptyList(),
-  @Schema(description = "List of disabilities for the person")
-  val disabilities: List<OffenderDisability>? = emptyList(),
-  @Schema(description = "List of disabilities for the person")
-  val interestsToImmigration: List<OffenderInterestToImmigration>? = emptyList(),
   @Schema(description = "Current belief and history of all beliefs for the person")
   val beliefs: List<OffenderBelief>?,
 )
@@ -401,174 +382,6 @@ data class Identifier(
   val verified: Boolean,
 )
 
-@Schema(description = "The data held in NOMIS about a address number")
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
-data class OffenderAddress(
-  @Schema(description = "Unique NOMIS Id of number")
-  val addressId: Long,
-  @Schema(description = "Flat name or number", example = "Apartment 3")
-  val flat: String?,
-  @Schema(description = "Premise", example = "22")
-  val premise: String?,
-  @Schema(description = "Street", example = "West Street")
-  val street: String?,
-  @Schema(description = "Locality", example = "Keighley")
-  val locality: String?,
-  @Schema(description = "Post code", example = "MK15 2ST")
-  val postcode: String?,
-  @Schema(description = "City")
-  val city: CodeDescription?,
-  @Schema(description = "County")
-  val county: CodeDescription?,
-  @Schema(description = "Country")
-  val country: CodeDescription?,
-  @Schema(description = "List of phone numbers for the address")
-  val phoneNumbers: List<OffenderPhoneNumber>?,
-  @Schema(description = "true if address validated by Post Office Address file??")
-  val validatedPAF: Boolean,
-  @Schema(description = "true if address not fixed. for example homeless")
-  val noFixedAddress: Boolean?,
-  @Schema(description = "true if this is the person's primary address")
-  val primaryAddress: Boolean,
-  @Schema(description = "true if this is used for mail")
-  val mailAddress: Boolean,
-  @Schema(description = "Free format comment about the address")
-  val comment: String?,
-  @Schema(description = "Date address was valid from")
-  val startDate: LocalDate?,
-  @Schema(description = "Date address was valid to")
-  val endDate: LocalDate?,
-  @Schema(description = "Usages for the address, also known as types")
-  val usages: List<OffenderAddressUsage>?,
-)
-
-@Schema(description = "The data held in NOMIS about a phone number")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderPhoneNumber(
-  @Schema(description = "Unique NOMIS Id of number")
-  val phoneId: Long,
-  @Schema(description = "The number")
-  val number: String,
-  @Schema(description = "Extension")
-  val extension: String?,
-  @Schema(description = "Phone type")
-  val type: CodeDescription,
-)
-
-@Schema(description = "The data held in NOMIS about a email address")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderEmailAddress(
-  @Schema(description = "Unique NOMIS Id of email address")
-  val emailAddressId: Long,
-  @Schema(description = "The email address", example = "john.smith@internet.co.uk")
-  val email: String,
-)
-
-@Schema(description = "Nationality details held against a booking")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderNationality(
-  @Schema(description = "The booking's unique identifier", example = "1234567")
-  val bookingId: Long,
-  @Schema(description = "The value of the profile info")
-  val nationality: CodeDescription,
-  @Schema(description = "The start date of the booking", example = "2020-07-17T12:34:56")
-  val startDateTime: LocalDateTime,
-  @Schema(
-    description = "The end date of the booking, or null if the booking is still active",
-    example = "2021-07-16T12:34:56",
-  )
-  val endDateTime: LocalDateTime?,
-  @Schema(
-    description = "Whether this is the latest booking or not. Note that latest does not imply active.",
-    example = "true",
-  )
-  val latestBooking: Boolean,
-)
-
-@Schema(description = "Further nationality details held against a booking")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderNationalityDetails(
-  @Schema(description = "The booking's unique identifier", example = "1234567")
-  val bookingId: Long,
-  @Schema(description = "Details on the nationality")
-  val details: String,
-  @Schema(description = "The start date of the booking", example = "2020-07-17T12:34:56")
-  val startDateTime: LocalDateTime,
-  @Schema(
-    description = "The end date of the booking, or null if the booking is still active",
-    example = "2021-07-16T12:34:56",
-  )
-  val endDateTime: LocalDateTime?,
-  @Schema(
-    description = "Whether this is the latest booking or not. Note that latest does not imply active.",
-    example = "true",
-  )
-  val latestBooking: Boolean,
-)
-
-@Schema(description = "Sexual orientation details held against a booking")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderSexualOrientation(
-  @Schema(description = "The booking's unique identifier", example = "1234567")
-  val bookingId: Long,
-  @Schema(description = "The value of the profile info")
-  val sexualOrientation: CodeDescription,
-  @Schema(description = "The start date of the booking", example = "2020-07-17T12:34:56")
-  val startDateTime: LocalDateTime,
-  @Schema(
-    description = "The end date of the booking, or null if the booking is still active",
-    example = "2021-07-16T12:34:56",
-  )
-  val endDateTime: LocalDateTime?,
-  @Schema(
-    description = "Whether this is the latest booking or not. Note that latest does not imply active.",
-    example = "true",
-  )
-  val latestBooking: Boolean,
-)
-
-@Schema(description = "Disability details held against a booking")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderDisability(
-  @Schema(description = "The booking's unique identifier", example = "1234567")
-  val bookingId: Long,
-  @Schema(description = "The value of the profile info")
-  val disability: Boolean,
-  @Schema(description = "The start date of the booking", example = "2020-07-17T12:34:56")
-  val startDateTime: LocalDateTime,
-  @Schema(
-    description = "The end date of the booking, or null if the booking is still active",
-    example = "2021-07-16T12:34:56",
-  )
-  val endDateTime: LocalDateTime?,
-  @Schema(
-    description = "Whether this is the latest booking or not. Note that latest does not imply active.",
-    example = "true",
-  )
-  val latestBooking: Boolean,
-)
-
-@Schema(description = "Interest to immigration details held against a booking")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderInterestToImmigration(
-  @Schema(description = "The booking's unique identifier", example = "1234567")
-  val bookingId: Long,
-  @Schema(description = "The value of the profile info")
-  val interestToImmigration: Boolean,
-  @Schema(description = "The start date of the booking", example = "2020-07-17T12:34:56")
-  val startDateTime: LocalDateTime,
-  @Schema(
-    description = "The end date of the booking, or null if the booking is still active",
-    example = "2021-07-16T12:34:56",
-  )
-  val endDateTime: LocalDateTime?,
-  @Schema(
-    description = "Whether this is the latest booking or not. Note that latest does not imply active.",
-    example = "true",
-  )
-  val latestBooking: Boolean,
-)
-
 @Schema(description = "Offender beliefs")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class OffenderBelief(
@@ -586,17 +399,6 @@ data class OffenderBelief(
   val comments: String? = null,
   @Schema(description = "Audit data associated with the records")
   val audit: NomisAudit,
-)
-
-@Schema(description = "Offender address usage")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class OffenderAddressUsage(
-  @Schema(description = "Offender belief id", example = "1123456")
-  val addressId: Long,
-  @Schema(description = "Address usage")
-  val usage: CodeDescription,
-  @Schema(description = "Whether the address usage is active")
-  val active: Boolean,
 )
 
 @Schema(description = "Update request for offender merge")
