bc95bace6f0f092b4db69c877ced07c74aa4c7c8
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-09-01 14:12:07 +0100

    SDIT-4106 Design upsert transfer schedule endpoint (#1987)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/movements/transfers/schedule/TransferScheduleModel.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/movements/transfers/schedule/TransferScheduleModel.kt
@@ -82,3 +82,75 @@ data class TransferScheduleWaitlist(
   @Schema(description = "Audit user's active caseload ID (modified user else create user)")
   val userActiveCaseloadId: String?,
 )
+
+@Schema(description = "Upsert transfer schedule out request")
+data class UpsertTransferScheduleOut(
+  @Schema(description = "Event ID")
+  val eventId: Long? = null,
+
+  @Schema(description = "Start time")
+  val startTime: LocalDateTime? = null,
+
+  @Schema(description = "Event sub type, aka movement reason")
+  val eventSubType: String,
+
+  @Schema(description = "Event status")
+  val eventStatus: String,
+
+  @Schema(description = "Comment")
+  val comment: String? = null,
+
+  @Schema(description = "Hidden Comment")
+  val hiddenComment: String? = null,
+
+  @Schema(description = "From prison")
+  val fromPrison: String,
+
+  @Schema(description = "To prison")
+  val toPrison: String? = null,
+
+  @Schema(description = "Cancellation reason")
+  val cancellationReasonCode: String? = null,
+
+  @Schema(description = "Escort code")
+  val escortCode: String? = null,
+
+  @Schema(description = "The waitlist")
+  val waitlist: UpsertTransferScheduleWaitlist? = null,
+)
+
+@Schema(description = "Upsert transfer schedule waitlist request")
+data class UpsertTransferScheduleWaitlist(
+  @Schema(description = "Requested date")
+  val requestDate: LocalDate,
+
+  @Schema(description = "Waitlist status")
+  val status: String,
+
+  @Schema(description = "The date the status was changed")
+  val statusDate: LocalDate,
+
+  @Schema(description = "The transfer priority")
+  val priority: String,
+
+  @Schema(description = "Is the transfer approved?")
+  val approved: Boolean,
+
+  @Schema(description = "Approved by user")
+  val approvedUserName: String? = null,
+
+  @Schema(description = "Cancellation reason")
+  val cancellationReasonCode: String? = null,
+
+  @Schema(description = "Comment")
+  val comment: String? = null,
+)
+
+@Schema(description = "Upsert transfer schedule response")
+data class UpsertTransferScheduleOutResponse(
+  @Schema(description = "Booking ID")
+  val bookingId: Long,
+
+  @Schema(description = "Event ID")
+  val eventId: Long,
+)
