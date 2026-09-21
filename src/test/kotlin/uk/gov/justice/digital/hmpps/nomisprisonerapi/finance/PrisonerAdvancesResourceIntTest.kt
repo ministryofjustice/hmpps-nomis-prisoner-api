7739b544a3aa2e3bba74389814185389b513d1f3
lbennett-moj <lucy.bennett1@justice.gov.uk>
2026-09-21 15:22:53 +0100

    sdit_4250 court event charge refresh to offender charge bug

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonerAdvancesResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisprisonerapi/finance/PrisonerAdvancesResourceIntTest.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.nomisprisonerapi.finance
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -143,7 +142,6 @@ class PrisonerAdvancesResourceIntTest : IntegrationTestBase() {
       }
     }
 
-    @Disabled
     @Test
     fun getPrisonerAdvances() {
       webTestClient.get().uri("/finance/prisoners/advances/${advance.id}")
